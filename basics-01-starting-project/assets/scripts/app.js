a2c4e932ac02ec74e216ad126a9bf582ed3f7873
Keshawa Methinda <kmethinda@gmail.com>
2025-02-18 18:40:14 +0530

    testing

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -5,6 +5,8 @@ let currentResult = defaultResult;
 
 currentResult = currentResult + 10 * 56 ;
 let calculationDescription = `(${defaultResult} + 10) * 3/2 - 10` ;
+let errorMessage = 'An error \n' + 
+                    'occured!';
 
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, errorMessage );
 
